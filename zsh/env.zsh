0ac7c65ceef5da10ef7f08859f93dc6a35cebfd0
matytan <tqtaylor@163.com>
2022-01-07 19:55:23 +0800

    init config

---
+++ zsh/env.zsh
@@ -0,0 +1,27 @@
+export PATH=$HOME/.local/bin:$PATH
+export XDG_CONFIG_HOME=$HOME/.config
+export LOCALBIN=$XDG_CONFIG_HOME/bin
+export PATH=$PATH:$LOCALBIN
+export LOCALPROG=$HOME/prog
+export GOPATH=$HOME/go
+export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOPATH/bin
+export PATH=$PATH:$HOME/.gem/ruby/2.6.0
+export PATH=$PATH:/snap/bin
+export PATH=$PATH:/usr/local/opt/node@12/bin
+#export TERM=xterm-256color
+export PATH=$PATH:$LOCALPROG/flutter/bin
+export PATH=$PATH:$LOCALPROG/flutter/bin/cache/dart-sdk/bin
+export PATH="$PATH":"$HOME/.pub-cache/bin"
+export FLUTTER_ROOT=$LOCALPROG/flutter
+export TERM=xterm-256color
+export TERM_ITALICS=true
+export RANGER_LOAD_DEFAULT_RC="false"
+#export TERM=screen-256color
+export EDITOR=nvim
+export ZSH_AUTOSUGGEST_USE_ASYNC=1
+export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
+export TERM=screen-256color-bce
+export LC_ALL=C
+# vim
+#export MYVIMRC = XDG_CONFIG_HOME/nvim/init.vim
